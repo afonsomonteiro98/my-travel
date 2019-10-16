94d998fe55abe4b765d02ca996d3b0c181688596
Afonso <afonsom98@gmail.com>
2019-10-16 12:16:31 +0100

    Quest 2

--- src/Components/Travel.js
+++ src/Components/Travel.js
@@ -3,11 +3,12 @@ import React from 'react';
 
 const Travel = props => (
     <div>
-        <p> Destination: {props.destination} </p>
-        <p> Country: {props.country} </p>
-        <img src={props.photo} />
-        <p> Distance: {props.distance} </p>
+        <p> Destination: {props.travelObject.destination} </p>
+        <p> Country: {props.travelObject.country} </p>
+        <img src={props.travelObject.photo} />
+        <p> Distance: {props.travelObject.distance} </p>
     </div>
 )
+
 
 export default Travel;
---
+++ src/Components/Travels.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Travel from "./Travel";
+
+const travels = [
+    {
+        destination:"Lisbon",
+        country:"Portugal",
+        photo:"https://www.flytap.com/-/media/Flytap/Campaigns/2017/NewDest/BB1024x553_LISBOA.jpg",
+        distance:"20km"
+    },
+    {
+        destination:"Porto",
+        country:"Portugal",
+        photo:"https://www.visitportugal.com/sites/default/files/styles/experiencias_detalhe/public/mediateca/Porto_d.jpg?itok=b0N9em9u",
+        distance:"250km"
+    },
+    {
+        destination:"Coimbra",
+        country:"Portugal",
+        photo:"https://imagens.publico.pt/imagens.aspx/1387934?tp=UH&db=IMAGENS&type=JPG&w=1920",
+        distance:"150km"
+    },
+    {
+        destination:"Aveiro",
+        country:"Portugal",
+        photo:"https://www.noticiasdeaveiro.pt/wp-content/uploads/2019/09/canal_central.jpg",
+        distance:"200km"
+    },
+    {
+        destination:"Guimarães",
+        country:"Portugal",
+        photo:"https://www.visitportugal.com/sites/www.visitportugal.com/files/mediateca/CG---4_660x371.jpg",
+        distance:"300km"        
+    }
+];
+
+const Travels = () => (
+        travels.map(travel => (
+            <Travel travelObject={travel}/>
+        ))
+);
+
+export default Travels;
